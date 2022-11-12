ff531b0db34d336af9092efee95c60b75e4fb77a
a1akris <alkrivitskiy@pm.me>
2022-11-12 12:15:27 +0200

    Workaround for waybar 0.9.14. Small neovim fix

--- nvim/lua/default/plug/luasnip.lua
+++ nvim/lua/default/plug/luasnip.lua
@@ -10,7 +10,6 @@ local s = ls.s
 local i = ls.insert_node
 local t = ls.text_node
 local c = ls.choice_node
-local f = ls.function_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 ls.config.set_config {

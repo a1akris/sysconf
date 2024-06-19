3de378bac623e6ea070f0e6a3731eafd749c1501
a1akris <alkrivitskiy@pm.me>
2024-06-19 23:57:52 +0300

    Add "Dynamic" neovim config selection

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,10 @@
-require "default"
+local config
+
+if vim.env.NVIM_CONFIG then
+    config = vim.env.NVIM_CONFIG
+else
+    config = "default"
+end
+
+require(config)
+vim.notify("Using config: " .. config)
